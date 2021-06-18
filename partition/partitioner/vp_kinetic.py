2888b000d3de72a9a4e6a48988007ee1a2327f6e
V.H. Chavez <gonza445@purdue.edu>
2021-06-18 13:22:14 -0400

    Need To use WuYang

--- partition/partitioner/vp_kinetic.py
+++ partition/partitioner/vp_kinetic.py
@@ -22,26 +22,13 @@ def vp_kinetic(self):
         # if self.optPart.verbose and self.iteration == 1:
         #     print("\t\t\tCalculating the vp kinetic")
 
-        # Define Initial guess for inverter
-        # Let us not do initial guess for now. 
-
-        # Sanity check
-        # if self.inverter.dt == None:
-        #     raise ValueError("Target Density Needed for inversion")
-
         # Individual vt
         for ifrag in self.frags:
             ifrag.V.vt =       - ( ifrag.V.vhxc + ifrag.V.vnuc)
-            ifrag.Plotter.vt = - ( ifrag.Plotter.vhxc + ifrag.Plotter.vnuc)
+            ifrag.Vnm.vt =     - ( ifrag.Vnm.vh + ifrag.Vnm.Vxca + ifrag.Vnm.V )
 
-        #Load Data to inverter
-        # nalpha = 0
-        # nbeta  = 0
-        # for ifrag in self.frags:
-        #     nalpha += ifrag.nalpha
-        #     nbeta += ifrag.nbeta
-        # self.inverter.nalpha = nalpha
-        # self.inverter.nbeta = nbeta
+            if self.plot_things:
+                ifrag.Plotter.vt = - ( ifrag.Plotter.vhxc + ifrag.Plotter.vnuc)
 
         # Set target components
         self.inverter.dt = [self.dfa, self.dfb]
@@ -63,28 +50,39 @@ def vp_kinetic(self):
         #     self.inverter.V += ifrag.V.Vnm
 
         # Invert protomolecular density
-        
+
+        _, vha = self.grid.esp(Da=self.dfa, Db=self.dfb, grid=self.grid.plot_points)
+        _, vha_dft = self.grid.esp(Da=self.dfa, Db=self.dfb, vpot=self.grid.vpot)
+        vc = self.grid.vxc(1,  Da=self.dfa, Db=self.dfb, grid=self.grid.plot_points)
+        vx = self.grid.vxc(12, Da=self.dfa, Db=self.dfb, grid=self.grid.plot_points)
+        vn = self.Plotter.vnuc
+
+        self.Plotter.vh0 = vha
+        self.Plotter.vxc0 = vx + vc
+        self.Plotter.vext0 = vn
+    
 
         if self.optPart.inv_method == 'oucarter':
             print("\t\t\t\tStartiting Ou Carter inversion")
-            #Calculate guess in each method(?)
-            _, vha = self.grid.esp(Da=self.dfa, Db=self.dfb, grid=self.grid.plot_points)
-            _, vha_dft = self.grid.esp(Da=self.dfa, Db=self.dfb, vpot=self.grid.vpot)
-            vc = self.grid.vxc(1,  Da=self.dfa, Db=self.dfb, grid=self.grid.plot_points)
-            vx = self.grid.vxc(12, Da=self.dfa, Db=self.dfb, grid=self.grid.plot_points)
-            vn = self.Plotter.vnuc
 
-            self.Plotter.vh0 = vha
-            self.Plotter.vxc0 = vx + vc
-            self.Plotter.vext0 = vn
+
             vxc_dft, vxc_plotter = self.inverter.invert(method=self.optPart.inv_method, opt_method=self.optPart.opt_method)
 
 
             if self.ref == 2:
-                vxc_plotter[0,:] -= vxc_plotter[0,0]
-                vxc_dft[0,:] -= vxc_dft[0,0]
-                vxc_plotter[1,:] -= vxc_plotter[1,0]
-                vxc_dft[1,:] -= vxc_dft[1,0]
+                print("Shifting")
+                vxc_plotter[0][:] -= vxc_plotter[0][0]
+                vxc_plotter[1][:] -= vxc_plotter[1][0]
+                vxc_dft[0][:] -= vxc_dft[0][0]
+                vxc_dft[1][:] -= vxc_dft[0][1]
+                # vxc_plotter[0,:] -= vxc_plotter[0,0]
+                # vxc_dft[0,:] -= vxc_dft[0,0]
+                # vxc_plotter[1,:] -= vxc_plotter[1,0]
+                # vxc_dft[1,:] -= vxc_dft[1,0]
+
+                vxc_plotter  = vxc_plotter[0][:]
+                vxc_dft      = vxc_dft[0][:]
+
             else:
                 vxc_plotter -= vxc_plotter[0]
                 vxc_dft -= vxc_dft[0]
@@ -93,8 +91,9 @@ def vp_kinetic(self):
             self.V.vxc       = vxc_dft
 
             #We displace potential to move it to zero, while we work on mu
-            self.Plotter.vt = - (vxc_plotter + vha     + vn)
-            self.V.vt       = - (vxc_dft     + vha_dft + self.V.vnuc) 
+            self.Plotter.vt = - (vxc_plotter   + vha     + vn)
+            self.V.vt       = - (vxc_dft       + vha_dft + self.V.vnuc) 
+            self.Vnm.vt     = - (self.inverter.Vxca_inv + self.inverter.va + self.Vnm.V )
 
             #Build KSmatrix of full system and all the energies.
             # Hartree
@@ -110,7 +109,7 @@ def vp_kinetic(self):
 
 
         elif self.optPart.inv_method == 'wuyang':
-            self.inverter.invert(method=self.optPart.inv_method, opt_method=self.optPart.opt_method)
+            self.inverter.invert(initial_guess='fermi_amaldi', method=self.optPart.inv_method, opt_method=self.optPart.opt_method)
             vt_plotter = self.grid.on_grid_ao(self.inverter.v, grid=self.grid.plot_points)
             vt         = self.grid.on_grid_ao(self.inverter.v, vpot=self.grid.vpot)
             self.V.vt = vt + vn
@@ -119,6 +118,9 @@ def vp_kinetic(self):
 
     # Finalize vp_kin
     for ifrag in self.frags:
-
         ifrag.V.vp_kin       = self.V.vt       - ifrag.V.vt
-        ifrag.Plotter.vp_kin = self.Plotter.vt - ifrag.Plotter.vt
+        ifrag.Vnm.vp_kin     = self.Vnm.vt - ifrag.Vnm.vt
+
+        if self.plot_things:
+            ifrag.Plotter.vp_kin = self.Plotter.vt - ifrag.Plotter.vt
+
